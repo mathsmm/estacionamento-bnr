88b2c9b7fe30549176f8925967b0dc759ffdc4d4
Matheus Marchi Moro <71672437+mathsmm@users.noreply.github.com>
2025-02-05 22:13:38 -0300

    Update RepositorioValorReferencia.cs

---
+++ Estacionamento/Data/Services/RepositorioValorReferencia.cs
@@ -0,0 +1,31 @@
+using Estacionamento.Data.Interfaces;
+using Estacionamento.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estacionamento.Data.Services
+{
+    public class RepositorioValorReferencia : IRepositorioValorReferencia
+    {
+        private readonly DataContext _contexto;
+
+        public RepositorioValorReferencia(DataContext contexto)
+        {
+            this._contexto = contexto;
+        }
+
+        public async Task<ValorReferencia[]> ObterTodas()
+        {
+            IQueryable<ValorReferencia> consulta = _contexto.ValorReferencia;
+
+            consulta = consulta.AsNoTracking()
+                               .OrderBy(x => x.DtIniVigencia);
+
+            return await consulta.ToArrayAsync();
+        }
+
+        public async Task<ValorReferencia> ObterPorData(DateTime data)
+        {
+            // Consultar valor que esteja entre uma data de início e uma data de fim
+        }
+    }
+}
